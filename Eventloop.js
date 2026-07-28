7d2b218b349327d3c1747e8380cc283a30a44760
Abhi Prajapati <abhiprajapati82180353@gmail.com>
2026-07-28 14:49:38 +0530

    first lab

---
+++ Eventloop.js
@@ -0,0 +1,58 @@
+// import { writeFile } from "fs/promises";
+
+// const f1 = () => {
+//     console.log("f1");
+// };
+
+// const f2 = () => {
+//     console.log("f2");
+// };
+
+// const f3 = () => {
+//     console.log("f3");
+// };
+
+// const writeData = async () => {
+//     await writeFile("note.txt", "I am fs module");
+//     console.log("file written");
+// };
+
+// const main = () => {
+//     console.log("main");
+
+//     writeData();
+
+//     setTimeout(f2, 0);
+//     setImmediate(f3);
+//     process.nextTick(f1);
+
+//     console.log("end");
+
+//     new Promise((resolve, reject) => {
+//         console.log("I am promise 1");
+//         resolve();
+//     });
+
+//     new Promise((resolve, reject) => {
+//         console.log("I am promise 2");
+//         resolve();
+//     });
+// };
+
+// main();
+
+
+// const f1=() =>{
+//     console.log("hello f1");
+
+// };
+// const f2=() =>{
+//     console.log("hello f2");
+
+// };
+// function main(){
+//     console.log("main");
+//     setTimeout(f1,0);
+//     f2();
+// }
+// main();
---
+++ index.js
@@ -0,0 +1,15 @@
+function sayHello(name) {
+    console.log(`Welcome to ${name}`);
+}
+
+const sayHi = (name) => {
+    console.log(`Welcome to ${name}`);
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+sayHello("Abhishek");
+sayHi("Abhi");
+console.log(`Sum of 2 and 5 is = ${sum(2, 5)}`);
