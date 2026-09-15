a0a6730c013beec3f52cffdd06260dc330e66d71
Abhi Prajapati <abhiprajapati82180353@gmail.com>
2026-09-15 21:10:43 +0530

    prg6

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,1 +1,42 @@
-import 
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === '/' && req.method === 'GET') {
+
+    res.statusCode = 200;
+    res.end("GET request");
+
+  }
+
+  else if (req.url === '/' && req.method === 'POST') {
+
+    res.statusCode = 200;
+    res.end("POST request");
+
+  }
+
+  else if (req.url === '/' && req.method === 'PUT') {
+
+    res.statusCode = 200;
+    res.end("PUT request");
+
+  }
+
+  else if (req.url === '/' && req.method === 'DELETE') {
+
+    res.statusCode = 200;
+    res.end("DELETE request");
+
+  }
+
+  else {
+
+    res.statusCode = 404;
+    res.end("request not found");
+
+  }
+
+}); // ⭐ VERY IMPORTANT
+
+server.listen(5000, () => console.log("prg6 is running"));
