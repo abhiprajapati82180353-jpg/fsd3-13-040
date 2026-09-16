ae301f1cf0d3349f8278a123c1c21d7e3bf8f294
Abhi Prajapati <abhiprajapati82180353@gmail.com>
2026-09-16 10:46:33 +0530

    hello

---
+++ lab1/hello.js
@@ -0,0 +1,16 @@
+function sayHello(name){
+    console.log(`welcome to ${name}`);
+}
+function sum(a,b){
+    return a+b;
+}
+const sayHi=(name)=>{
+    console.log(`welcome to ${name}`);
+}
+const sum2=(a,b)=>{
+    return a+b;
+}
+sayHello(`Amit singh`)
+console.log(`Sum of 2 and 5 is ${sum(2,5)}`);
+sayHi("Altaf Raja");
+console.log(`sum of 3 and 6 is ${sum2(3,6)}`);
